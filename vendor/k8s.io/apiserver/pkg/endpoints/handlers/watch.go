3fdc107249df3128753bf38680584b1b10c3a308
Predrag Janosevic <predrag.janosevic@exoscale.com>
2026-02-27 09:47:43 +0000

    Revert k8s.io dependencies (#31)
    Latest k8s.io libraries (0.35.1) don't work with cert-manager lib.
    Upstream example webhook we forked from is broken as well (we are
    matching their dep versions).
    I tested by gradually downgrading 0.34.4 seems to be last that works.
    
    I expect this to fix #30

--- vendor/k8s.io/apiserver/pkg/endpoints/handlers/watch.go
+++ vendor/k8s.io/apiserver/pkg/endpoints/handlers/watch.go
@@ -41,6 +41,9 @@ import (
 	compbasemetrics "k8s.io/component-base/metrics"
 )
 
+// nothing will ever be sent down this channel
+var neverExitWatch <-chan time.Time = make(chan time.Time)
+
 // timeoutFactory abstracts watch timeout logic for testing
 type TimeoutFactory interface {
 	TimeoutCh() (<-chan time.Time, func() bool)
@@ -55,8 +58,7 @@ type realTimeoutFactory struct {
 // and a cleanup function to call when this happens.
 func (w *realTimeoutFactory) TimeoutCh() (<-chan time.Time, func() bool) {
 	if w.timeout == 0 {
-		// nothing will ever be sent down this channel
-		return nil, func() bool { return false }
+		return neverExitWatch, func() bool { return false }
 	}
 	t := time.NewTimer(w.timeout)
 	return t.C, t.Stop
